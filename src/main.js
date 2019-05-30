6c6d45aacc614468ef07a13b6a4aabfe946ee101
fuliang <fuliang27209@163.com>
2019-05-30 16:43:40 +0800

    mock learing

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+require('./mock.js')
+
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.filter('getYMD', function(input) {
+  return input.split(' ')[0];
+})
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Hello from './components/Hello'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
